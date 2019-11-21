fix wrong size in softmax

bombaAgent.py
@@ -34,8 +34,10 @@ class Policy(torch.nn.Module):
         x_ac = F.relu(x_ac)
         x_mean = self.fc2_mean(x_ac)
 
-        x_probs = F.softmax(x_mean, dim=-1)
-        dist = Categorical(x_probs)
+        x_probs = F.softmax(x_mean, dim=-1) 
+        # Normalize the number of the vector in order to have their sum equal to 1
+        # dim=-1 means that it will take all the 3 values and normalise them
+        dist = Categorical(x_probs) # Example of output: Categorical(probs: torch.Size([3]))
 
         x_cr = self.fc1_critic(x)
         x_cr = F.relu(x_cr)

train_pong_ai.py
@@ -144,7 +144,7 @@ for it in range(0,episodes):
         ts = torch.FloatTensor(vs[action_batch.cpu().numpy()]) # [n_actions, 3]
 
         _, __, logits = myplayer.policy.forward(d_obs_batch)
-        r = torch.sum(F.softmax(logits, dim=3) * ts.to(device), dim=1) / action_prob_batch
+        r = torch.sum(F.softmax(logits, dim=-1) * ts.to(device), dim=1) / action_prob_batch
         loss1 = r * advantage_batch
         loss2 = torch.clamp(r, 1-myplayer.eps_clip, 1+myplayer.eps_clip) * advantage_batch
         loss = -torch.min(loss1, loss2)