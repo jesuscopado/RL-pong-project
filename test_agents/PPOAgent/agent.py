9b680fbcc586f34eb3988fd9bcfc888df333c13f
jesuscopado <jcr.22@hotmail.com>
2019-12-04 16:45:53 +0200

    Action space back to 2

--- test_agents/PPOAgent/agent.py
+++ test_agents/PPOAgent/agent.py
@@ -94,7 +94,7 @@ class Agent(object):
     def __init__(self, train_device="cpu"):
         self.train_device = train_device
         self.input_dimension = 100 * 100  # downsampled by 2 -> 100x100 grid
-        self.action_space = 3
+        self.action_space = 2
         self.policy = Policy(self.action_space, self.input_dimension).to(self.train_device)
         # self.policy = PolicyConv(self.action_space, 128).to(self.train_device)
         self.optimizer = torch.optim.Adam(self.policy.parameters(), lr=1e-3)
